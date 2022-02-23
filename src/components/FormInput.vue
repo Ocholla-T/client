baf29763dde0af97861ecd3cfe780fa5b088e5e2
Ocholla-T <tonnyocholla8@gmail.com>
2022-02-23 13:50:43 +0300

    add form input and its styling

---
+++ src/components/FormInput.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({ placeholder: String, title: String, icon: String, inputType: String });
+</script>
+
+<template>
+  <label>{{ title }}</label>
+  <div class="input">
+    <input class="input__field" :type="inputType" :placeholder="placeholder" />
+    <img class="input__icon" :src="icon" alt="icon" />
+  </div>
+</template>
+
+<style lang="scss">
+@use '../assets/styles/components/form-input';
+</style>
