fc4904d8be5cf59d902725af1a0922e0d2a19bc1
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-08 17:39:46 +0300

    ADD: props from Signup and Login views

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -1,12 +1,28 @@
 <script setup>
-defineProps({ placeholder: String, title: String, icon: String, inputType: String });
+defineProps({
+  placeholder: { type: String, required: true },
+  title: { type: String, required: true },
+  icon: { type: String, required: true },
+  inputType: { type: String, required: true },
+  modelValue: String,
+});
+defineEmits(['update:modelValue']);
 </script>
 
 <template>
-  <label>{{ title }}</label>
-  <div class="input">
-    <input class="input__field" :type="inputType" :placeholder="placeholder" />
-    <img class="input__icon" :src="icon" alt="icon" />
+  <div>
+    <label>{{ title }}</label>
+    <div class="input">
+      <input
+        class="input__field"
+        :type="inputType"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        required
+      />
+      <img class="input__icon" :src="icon" alt="icon" />
+    </div>
   </div>
 </template>
 
