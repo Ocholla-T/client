3d1a4256c93708ffd2fc717c6190293091e2dbc7
Ocholla-T <tonnyocholla8@gmail.com>
2022-03-08 17:39:01 +0300

    ADD: Error message modal card

---
+++ src/components/ErrorMessageCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+let isOpen = ref(true);
+
+function close() {
+  if (isOpen.value == true) {
+    return (isOpen.value = false);
+  }
+}
+</script>
+
+<template>
+  <div v-if="isOpen" class="card">
+    <p>
+      <slot name="error_message"> </slot>
+    </p>
+    <img
+      class="card__icon"
+      src="@/assets/images/close-circle-outline.svg"
+      alt="close"
+      @click="close"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+@use '@/assets/styles/components/error-message-card.scss';
+</style>
